ecb9e3cd6bf16dd8250572f9d0b8262fd68a2758
cucuatok93 <35328742+cucuatok93@users.noreply.github.com>
2018-01-14 14:46:32 +0800

    Update naagin.sh

--- naagin.sh
+++ naagin.sh
@@ -28,7 +28,7 @@ echo -e "\e[38;5;6m     *                Channel: CuCuAtoK_TeaM                 
 echo -e "\e[38;5;6m     *                Whatsapp: -                            *"
 echo -e "\e[38;5;6m     *                Telegram: @Cucu_atok                   *"
 echo -e "\e[38;5;6m     ========================================================="
-echo -e "\e[38;5;6m     *                AUTOSCRIPT VPS 2018                    *"
+echo -e "\e[38;5;196m     *                AUTOSCRIPT VPS 2018                    *"
 echo -e "\e[38;5;6m     ========================================================="
 # check registered ip
 wget -q -O IP "https://raw.githubusercontent.com/cucuatok93/teamcck/master/IP.txt"
